d431c2cf6f6bf9a0a2dff3d6011886c85e9bd0b3
chukkwagon <mcbride.charles4@gmail.com>
2015-04-21 16:43:17 -0500

    client side google signin basic functionality

--- client/users/auth.js
+++ client/users/auth.js
@@ -5,34 +5,8 @@ angular.module('nomLater.signup', ['ngRoute'])
 .controller('SignUpCtrl', function ($scope, $window, $location, Users) {
   $scope.user = {};
 
-  //for some reason, signedIn did not work when I just had it as a plain variable, so I had to make it a function
   $scope.signedIn = function() {
     return !!$window.localStorage['com.corgi']
-  };
-
-  $scope.signin = function () {
-    Users.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.corgi', token);
-        $location.path('/');
-      })
-      .catch(function (error) {
-        if(error.status === 401){
-        alert("Incorrect Username or Password.")
-        }
-        console.error(error);
-      });
-  };
-
-  $scope.signup = function() {
-    Users.signup($scope.user)
-      .then(function(token){
-        $window.localStorage.setItem('com.corgi', token);
-        $location.path('/');
-      })
-      .catch(function(error){
-        console.log(error);
-    });
   };
 
   $scope.signout = function() {
@@ -42,17 +16,13 @@ angular.module('nomLater.signup', ['ngRoute'])
 
   $scope.signInButton = function() {
     auth2.grantOfflineAccess({'redirect_uri' : 'postmessage'}).then(function(resp) {
-      console.log(resp);
-      $http({
-        method: 'POST',
-        url: '/api/users/signin',
-        data: resp.code
-      }).then(function (token) {
-        $window.localStorage.setItem('com.corgi', token);
-        $location.path('/');
-      })
-      .catch(function(error) {
-        console.log(error);
+      Users.signin(resp)
+        .then(function (token) {
+          $window.localStorage.setItem('com.corgi', token);
+          $location.path('/');
+        })
+        .catch(function(error) {
+          console.log(error);
       });
     });
   }
